99cb9d80ff51fb1174be480cfdeb23845511476a
Jesus Medina <jesusmedina@MacBook-Air-de-Jesus.local>
2025-07-25 21:33:23 -0400

    Fix SyntaxError: Unexpected end of input in views.router.js

--- src/routes/views.router.js
+++ src/routes/views.router.js
@@ -1,35 +1,34 @@
-import { Router } from 'express';
-
-const router = Router();
-
-router.get('/', async (req, res) => {
-  const { page = 1, limit = 10, sort, query } = req.query;
-  const products = await productManager.getAll({ page, limit, sort, query });
-  res.render('home', { 
-    products: products.docs,
-    hasPrevPage: products.hasPrevPage,
-    hasNextPage: products.hasNextPage,
-    prevPage: products.prevPage,
-    nextPage: products.nextPage,
-    page: products.page,
-    totalPages: products.totalPages
-  });
-});
-
-router.get('/realtimeproducts', async (req, res) => {
-  const products = await productManager.getAll({});
-  res.render('realTimeProducts', { products: products.docs });
-});
-
-router.get('/products/:pid', async (req, res) => {
-  const product = await productManager.getById(req.params.pid);
-  res.render('product', { product });
-});
-
-router.get('/carts/:cid', async (req, res) => {
-  const cart = await cartManager.getById(req.params.cid);
-  res.render('cart', { cart });
-});
-
 export default (productManager, cartManager, express) => {
   const router = express.Router();
+
+  router.get('/', async (req, res) => {
+    const { page = 1, limit = 10, sort, query } = req.query;
+    const products = await productManager.getAll({ page, limit, sort, query });
+    res.render('home', { 
+      products: products.docs,
+      hasPrevPage: products.hasPrevPage,
+      hasNextPage: products.hasNextPage,
+      prevPage: products.prevPage,
+      nextPage: products.nextPage,
+      page: products.page,
+      totalPages: products.totalPages
+    });
+  });
+
+  router.get('/realtimeproducts', async (req, res) => {
+    const products = await productManager.getAll({});
+    res.render('realTimeProducts', { products: products.docs });
+  });
+
+  router.get('/products/:pid', async (req, res) => {
+    const product = await productManager.getById(req.params.pid);
+    res.render('product', { product });
+  });
+
+  router.get('/carts/:cid', async (req, res) => {
+    const cart = await cartManager.getById(req.params.cid);
+    res.render('cart', { cart });
+  });
+
+  return router;
+};
